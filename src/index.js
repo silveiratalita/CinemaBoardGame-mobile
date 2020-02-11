34c2f98603f27e7c48303ab7a02fa1f4d2bba89a
Talita <silveira.tacristina@gmail.com>
2020-02-10 21:44:26 -0300

    reactotron

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import './config/ReactotronConfig';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Login from './src/Pages/Login';
+import Register from './src/Pages/Register';
+import Home from './src/Pages/Home';
+import CreateGameRoom from './src/Pages/CreateGameRoom';
+import Room from './src/Pages/Room';
+import GameRoom from './src/Pages/GameRoom/index';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return <Room />;
+};
+
+export default App;
